ec48ef864f2f51904b26769c399ca278cfe07dc4
Society42 <104001559+Society42@users.noreply.github.com>
2025-01-08 19:50:35 +0000

    Delete StaffPage directory

--- StaffPage/Secure/deleted-account.js
+++
@@ -1,3 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const reason = urlParams.get('reason');
-document.getElementById('deleted-reason').textContent = reason || 'Your account has been deleted due to a violation of policies.';
